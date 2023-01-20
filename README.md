3ef8775d0978c1394126c054afd46cf6e6c4c43c
Heidi Ganac Nicholson <heidiganac@gmail.com>
2023-01-19 19:37:32 -0600

    created functions to manipulate buttons, disabled buttons

--- README.md
+++ README.md
@@ -6,19 +6,16 @@ Fight mutant fungus villains, with jQuery!
 
 It is you versus the Freaky Fungus, an evil mycological menace from mars. Use your jQuery skills to dispatch attacks against your enemy. To win, the Freaky Fungus must have 0 hit points (HP) before you are out of attack points (AP).
 
-
-
 ## Requirements
 
-**Handle click events:** 
+**Handle click events:**
 
 When you click an attack button:
 
--  update _state_ variable(s) to make the Freaky Fungus lose hit points (HP), and to reduce your attack points (AP). 
-- See [Attacks](#attacks) below, for the AP and HP values of each attack
+- update _state_ variable(s) to make the Freaky Fungus lose hit points (HP), and to reduce your attack points (AP).
+- See [Attacks](#a9acks) below, for the AP and HP values of each attack
 - State may be held in one our more variables of your choosing
 - HP and AP values may not be negative (set to zero, if they would otherwise be negative)
-
 
 **Render state changes to the DOM**
 
@@ -26,14 +23,15 @@ When you click an attack button:
 - Update the text above the enemy fungus (eg, "100 HP")
 - If the Freaky Fungus runs out of HP, the monster is dead and you win! Replace the `walk` class with a `dead` class on the freaky-fungus element, to make them fall over and die.
 - If you run out of AP, the monster wins and humanity is doomed 😢 Replace the `walk` class with a `jump` class on the freaky-fungus element, to make them jump for the glory of the fungal race.
-  - You may no longer attack, if AP is `0`. Give all of the attack buttons a [`disabled`](https://www.w3schools.com/tags/att_button_disabled.asp) attribute, so they may no longer be used. 
+  - You may no longer attack, if AP is `0`. Give all of the attack buttons a [`disabled`](asphttps://www.w3schools.com/tags/att_button_disabled.) attribute, so they may no longer be used.
 
 ## Stretch Goals
 
 **Progress Bar**
 
 When state changes:
-  - Update the value of each `<progress>` element
+
+- Update the value of each `<progress>` element
 
 > Hint: the jQuery `.val()` method updates the `value` attribute of HTML elements!
 
@@ -45,14 +43,12 @@ If the Freaky Fungus' HP falls below 50, have it regenerate 1 HP every second.
 
 > HINT: Look up the built in `setInterval()` function!
 
-
 ## Attacks
 
 ### ![arcane scepter](./images/arcane-sceptre.png) Arcane Scepter
 
 - **AP Cost:** 12
 - **HP Damage:** 14
-
 
 ### ![entangle](./images/entangle-3.png) Entangle
 
@@ -69,10 +65,10 @@ If the Freaky Fungus' HP falls below 50, have it regenerate 1 HP every second.
 - **AP Cost:** 33
 - **HP Damage:** 25
 
-
 ## Attributions
 
 Game art is freely available from [OpenGameArt.org](https://opengameart.org/):
+
 - https://opengameart.org/content/animated-mushroom-monster-pixel-art
 - https://opengameart.org/content/attack-icons-wesnoth
 - https://opengameart.org/content/dark-forest
--- script.js
+++ script.js
@@ -5,14 +5,100 @@ $(document).ready(onReady);
 // Example:
 // let fungusHP = 100;
 
+let fungusHPMeter = 100;
+let attackAPMeter = 100;
+const arcaneSceptre = {name:'arcane-sceptre', damage: 14, cost: 12}
+const entangle = {name:'entangle', damage: 9, cost: 23};
+const dragon = {name:'dragon-blade', damage: 47, cost: 38};
+const starFire = {name:'star-fire', damage: 25, cost: 33};
+
 function onReady() {
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
-    
-    // 🧠 Remember
+ // EVENT
+    // Handle the attack button click event
+ 
+ $('.attack-btn.arcane-sceptre').on('click', attackBtnArcaneOnClick); 
+ $('.attack-btn.entangle').on('click', attackBtnEntangleOnClick);
+ $('.attack-btn.dragon-blade').on('click', attackBtnDragonOnClick);
+ $('.attack-btn.star-fire').on('click', attackBtnStarFireOnClick);
+
+}
+ // STATE
+    // update the state variables
+    // Freaky Fungus needs to lose HP pts
+function attackBtnArcaneOnClick() {
+    console.log('results:', attackAPMeter - arcaneSceptre.cost, fungusHPMeter - arcaneSceptre.damage);
+    if (attackAPMeter > arcaneSceptre.cost) {
+        fungusHPMeter = fungusHPMeter - arcaneSceptre.damage
+        attackAPMeter = attackAPMeter - arcaneSceptre.cost;
+    }
+    render();
+} 
+function attackBtnEntangleOnClick() {
+    console.log('results:' );
+    if (attackAPMeter > entangle.cost) {
+    fungusHPMeter = fungusHPMeter - entangle.damage;
+    attackAPMeter = attackAPMeter - entangle.cost;
+    }
+    render();
+}
+function attackBtnDragonOnClick() {
+    console.log('results:');
+    if (attackAPMeter > dragon.cost) {
+    fungusHPMeter = fungusHPMeter - dragon.damage;
+    attackAPMeter = attackAPMeter - dragon.cost;
+    }
+    render();
+}
+function attackBtnStarFireOnClick() {
+    console.log('results:');
+    if (attackAPMeter > starFire.cost) {
+    fungusHPMeter = fungusHPMeter - starFire.damage;
+    attackAPMeter = attackAPMeter - starFire.cost;
+    }
+    render();
+}
+ // RENDER 
+    // rendering to the DOM
+function render() {
+    $('#ap-meter').val(attackAPMeter);
+    $('#hp-meter').val(fungusHPMeter);
+    $('.ap-text').text(attackAPMeter + ' AP');
+    $('.hp-text').text(fungusHPMeter + ' HP');
+
+    if (fungusHPMeter <= 0) {
+        $('.freaky-fungus').removeClass('walk');
+        $('.freaky-fungus').addClass('dead');
+    }
+    if (attackAPMeter <= 0) {
+        $('.freaky-fungus').addClass('jump');
+        $('.freaky-fungus').removeClass('walk');
+    }
+
+    let canCastSceptre = attackAPMeter >= arcaneSceptre.cost
+    let canEntangle = attackAPMeter >= entangle.cost
+    let canDragon = attackAPMeter >= dragon.cost
+    let canStarFire = attackAPMeter >= starFire.cost
+
+    if (!canCastSceptre) {
+        $('.attack-btn.arcane-sceptre').prop('disabled', true);
+    }
+    if (!canEntangle) {
+        $('.attack-btn.entangle').prop('disabled', true);
+    }
+    if (!canDragon) {
+        $('.attack-btn.dragon-blade').prop('disabled', true);
+    }
+    if (!canStarFire) {
+        $('.attack-btn.star-fire').prop('disabled', true);
+    }
+}
+
+  // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
-}
+
